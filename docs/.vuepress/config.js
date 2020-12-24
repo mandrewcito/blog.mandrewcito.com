a13ce5f373af5e1e794fe8d5e2e68711475c320e
mandrewcito <anbaalo@gmail.com>
2020-12-25 00:48:07 +0100

    vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    themeConfig: {
+      logo: '/assets/img/arya.jpg',
+        navbar: true,
+        nav: [
+          { text: 'Home', link: '/' },
+          { text: 'Guide', link: '/guide/' },
+
+        ]
+    }
+}  
